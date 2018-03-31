bfcf4c87e15ce50441308ca768ea56a678a02cfa
Marcus Christiansen <mefchristiansen@gmail.com>
2018-03-31 23:31:12 +0000

    added show note toggle and button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 class Nav extends React.Component {
 	render() {
+		const { toggleNote, showNote } = this.props; // toggleNote function was passed from App to Nav component as a prop
+
 		return (
 
 			<div className="nav-container">
 				<div className='nav-logo'>Note</div>
-				<div className='nav-button'>
-					+ Note
+				<div className='nav-button' onClick={() => toggleNote()}>
+					{ showNote ? 'Cancel' : '+ Note' }
 				</div>
 			</div>
 		);
