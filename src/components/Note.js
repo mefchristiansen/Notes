6232fd6af3e8988d77bf134d39e27f8c9cd1e396
Marcus Christiansen <mefchristiansen@gmail.com>
2018-04-01 00:28:28 +0000

    rendering NoteCard components

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Note extends React.Component {
 	render() {
 		return (
-			<div className="list-container">
+			<div className="note-container">
 				Note Component
 			</div>
 		)
