b8284399d287952790c8a39365ea2ce40381b22d
Marcus Christiansen <mefchristiansen@gmail.com>
2018-04-01 18:26:36 +0000

    render note card content

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 class NoteCard extends React.Component {
 	render() {
+		const { note } = this.props;
+
 		return (
 			<div className='note-card-container'>
-				NoteCard Component
+				<div className='note-card-title'>
+					{note.title}
+				</div>
+				<div className='note-card-content'>
+					{note.content}
+				</div>
+				<div className='note-card-delete'>
+					<i className='material-icons'>close</i>
+				</div>
+				<div className='note-card-edit'>
+					<i className='material-icons'>mode_edit</i>
+				</div>
 			</div>
 
 		);
