db5b3364ce03fd9e60b863a9f71bde466c955cbe
Marcus Christiansen <mefchristiansen@gmail.com>
2018-04-01 20:15:35 +0000

    toggle add tag on click

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
     this.state = {
       showNote: false,
       notes: [],
-      note: {}
+      note: {},
+      newTag: false
     };
   }
 
@@ -40,7 +41,7 @@ class App extends Component {
     .catch((err) => console.log(err.response.data));
   }
 
-  performSubmissionRequest = (data, id) => {
+  performSubmissionRequest(data, id) {
     if (id) {
       return axios.patch(urlFor(`notes/${id}`), data);
     } else {
@@ -66,8 +67,14 @@ class App extends Component {
     .catch((err) => console.log(err.response.data));
   }
 
+  showTagForm = () => {
+    this.setState({
+      newTag: true
+    });
+  }
+
   render() {
-    const { showNote, notes, note } = this.state; // Destructure assignment
+    const { showNote, notes, note, newTag } = this.state; // Destructure assignment
 
     return (
       <div className="App">
@@ -76,6 +83,8 @@ class App extends Component {
           <Note
             note={note}
             submitNote={this.submitNote}
+            showTagForm={this.showTagForm}
+            newTag={newTag}
           />
           :
           <List
